c56218629776634e1ce2aa5c269c5668f6a23b2c
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-10-11 16:55:12 -0600

    youtube api added and video list rendered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import youtube from "./components/apis/youtube";
+import VideoList from "./components/VideoList";
 
-function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-      
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    videos: [],
+  };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({videos: response.data.items});
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,46 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = {term: ""};
+
+  onInputChange = (e) => {
+    // Taking the state of the input value
+    this.setState({term: e.target.value});
+  };
+
+  onFormSubmit = (e) => {
+    // Stops the page from rerendering
+    e.preventDefault();
+
+    this.props.onFormSubmit(this.state.term);
+  };
+
   render() {
     return (
-      <div className="ui segment"> 
-        <form className="ui form" >
-            <div className="ui fluid action input">
-              <div className='field'>
-                {/* <label>Video Search</label> */}
-                <input type="text" placeholder="Search Here"/>
-                 <div className="ui button">Search</div>
-              </div>
-             
+      <div className=" search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="ui fluid action input">
+            <div className="field">
+              <label>Video Search</label>
+              <input
+                type="text"
+                placeholder="Search Here"
+                value={this.state.term}
+                onChange={this.onInputChange}
+              />
+              {/* <div
+                className="ui button"
+                value={this.state.term}
+                onClick={this.onInputChange}
+              >
+                Search
+              </div> */}
             </div>
-          </form>
+          </div>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default SearchBar; 
+export default SearchBar;
